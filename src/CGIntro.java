8a8c6de9048677d95d9f75223cae8e04faa9fa6b
Xiran Yan <u7167582@anu.edu.au>
2020-10-26 18:53:21 +0800

    add shadows

--- src/CGIntro.java
+++ src/CGIntro.java
@@ -49,7 +49,9 @@ public class CGIntro implements GLEventListener {
 	int[] starttime=new int[10];//0-10(10 types)
 	boolean rote=false;
 
-	float lightpos[] = { 5f, 10f, 10f, 1.0f };
+	float lightpos[] = { 3f, 4f, 10f, 1.0f };
+	float ground[] = { 0.0f, 0.0f, -3.0f };
+	float groundnormal[] = { 0.0f, 0.0f, -1.0f };
 
 	public static void main(String[] args) throws IOException, UnsupportedAudioFileException {
 		new CGIntro();
@@ -157,7 +159,7 @@ public class CGIntro implements GLEventListener {
 		gl2.glPushMatrix();
 		//if starttime haven't come
 		if (speedDown < 0) {
-			gl2.glTranslatef(startpos[num] + (speedLeftOrRight * direct[num]), (10.0f) , 0.0f);
+			gl2.glTranslatef(0, 20 , 0.0f);
 		} else {
 			gl2.glTranslatef(startpos[num]+(speedLeftOrRight*direct[num]), (10.0f)-(speedDown), 0.0f);
 		}
@@ -165,6 +167,25 @@ public class CGIntro implements GLEventListener {
 		drawSphere(gl2,glu,glut);
 		gl2.glPopMatrix();
 
+		gl2.glPushMatrix();
+		if (speedDown < 0) {
+			gl2.glTranslatef(0,20,0);
+		} else {
+			gl2.glTranslatef(startpos[num]+(speedLeftOrRight*direct[num]), (10.0f)-(speedDown), 0.0f);
+		}
+		gl2.glEnable(GL2.GL_POLYGON_OFFSET_FILL);
+		gl2.glDisable(GL2.GL_LIGHTING);
+		gl2.glPolygonOffset(-1.0f, -1.0f);
+		gl2.glPushMatrix();
+		float[] rgb=Color.darkGray.getColorComponents(null);
+		gl2.glColor3d(rgb[0],rgb[1],rgb[2]);
+		projectShadow(gl2, ground, groundnormal, lightpos);
+		//gl2.glColor3d(1,0,0);
+		drawShadow(gl2,glu,glut);
+		gl2.glPopMatrix();
+		gl2.glDisable(GL2.GL_POLYGON_OFFSET_FILL);
+		gl2.glEnable(GL2.GL_LIGHTING);
+		gl2.glPopMatrix();
 		//time change from 0.0 to 9.95, every step increase 0.05
 		if (time < introTime) {
 			//System.out.println(time);
@@ -189,7 +210,17 @@ public class CGIntro implements GLEventListener {
 		glu.gluSphere(sphere,0.5,50,50);
 		gl2.glPopMatrix();
 	}
-
+	public void drawShadow(GL2 gl2, GLU glu, GLUT glut) {
+		object.disable(gl2);
+		gl2.glPushMatrix();
+		GLUquadric sphere=glu.gluNewQuadric();
+		glu.gluQuadricDrawStyle(sphere,GLU.GLU_FILL);
+		glu.gluQuadricTexture(sphere, true);
+		glu.gluQuadricNormals(sphere, GLU.GLU_SMOOTH);
+		glu.gluSphere(sphere,0.5,50,50);
+		gl2.glPopMatrix();
+		object.enable(gl2);
+	}
 	/**
 	 * draw the background:
 	 * @auther: Xiran Yan(Siya)
@@ -201,14 +232,14 @@ public class CGIntro implements GLEventListener {
 		cgtexture.bind(gl2);
 		gl2.glEnable(GL2.GL_TEXTURE_2D);
 		gl2.glBegin(GL2.GL_POLYGON);
-		gl2.glVertex3d(-20.0, -20.0, -10.0);
-		gl2.glTexCoord3d(0.0,0.0,0.0);
-		gl2.glVertex3d(-20.0, 20.0, -10.0);
-		gl2.glTexCoord3d(1.0,0.0,0.0);
-		gl2.glVertex3d(20.0, 20.0, -10.0);
-		gl2.glTexCoord3d(1.0,1.0,0.0);
-		gl2.glVertex3d(20.0, -20.0, -10.0);
-		gl2.glTexCoord3d(0.0,1.0,0.0);
+		gl2.glVertex3d(-20.0, -20.0, ground[2]);
+		gl2.glTexCoord3d(0.0,0.0,ground[2]);
+		gl2.glVertex3d(-20.0, 20.0, ground[2]);
+		gl2.glTexCoord3d(1.0,0.0,ground[2]);
+		gl2.glVertex3d(20.0, 20.0, ground[2]);
+		gl2.glTexCoord3d(1.0,1.0,ground[2]);
+		gl2.glVertex3d(20.0, -20.0, ground[2]);
+		gl2.glTexCoord3d(0.0,1.0,ground[2]);
 		gl2.glEnd();
 		gl2.glDisable(GL2.GL_TEXTURE_2D);
 		gl2.glPopMatrix();
@@ -266,13 +297,18 @@ public class CGIntro implements GLEventListener {
 		gl2.glEnable(GL2.GL_LIGHT0);
 		gl2.glLightfv(GL2.GL_LIGHT1, GL2.GL_AMBIENT, new float[]{ 0.3f, 0.3f,0.3f, 1.0f }, 0);
 		gl2.glLightfv(GL2.GL_LIGHT1, GL2.GL_DIFFUSE, new float[]{ 0.7f, 0.7f, 0.7f, 1.0f }, 0);
-		gl2.glLightfv(GL2.GL_LIGHT1,GL2.GL_SPECULAR, new float[]{ 0.5f, 0.5f, 0.5f, 1.0f },0);
+		gl2.glLightfv(GL2.GL_LIGHT1,GL2.GL_SPECULAR, new float[]{ 0.6f, 0.6f, 0.6f, 1.0f },0);
 		gl2.glLightfv(GL2.GL_LIGHT1, GL2.GL_POSITION, lightpos, 0);
 		gl2.glEnable(GL2.GL_LIGHT1);
+		gl2.glLightfv(GL2.GL_LIGHT2, GL2.GL_AMBIENT, new float[]{ 0.3f, 0.3f,0.3f, 1.0f }, 0);
+		gl2.glLightfv(GL2.GL_LIGHT2, GL2.GL_DIFFUSE, new float[]{ 0.7f, 0.7f, 0.7f, 1.0f }, 0);
+		gl2.glLightfv(GL2.GL_LIGHT2,GL2.GL_SPECULAR, new float[]{ 0.6f, 0.6f, 0.6f, 1.0f },0);
+		gl2.glLightfv(GL2.GL_LIGHT2, GL2.GL_POSITION, lightpos, 0);
+		gl2.glEnable(GL2.GL_LIGHT2);
 
 		try {
 			cgtexture = TextureIO.newTexture(new File("src/images/backgound.jpg"), true);
-			object = TextureIO.newTexture(new File("src/images/strawberry_1.jpg"), true);
+			object = TextureIO.newTexture(new File("src/images/strawberry_2.jpg"), true);
 			object.enable(gl2);
 			cgtexture.enable(gl2);
 			//cgtextureAspect = ((float) cgtexture.getImageWidth()) / cgtexture.getImageHeight();
@@ -286,7 +322,7 @@ public class CGIntro implements GLEventListener {
 	 * using glusphere draw a simple sphere:
 	 * @auther: Xiran Yan(Siya)
 	 * @uid: u7167582
-	 * todo setup the shadow for objects
+	 * todo modify the shadows for objects
 	 */
 	public void display(GLAutoDrawable dr) {
 		GL2 gl2 = dr.getGL().getGL2();
@@ -301,29 +337,6 @@ public class CGIntro implements GLEventListener {
 			transAndRotate(gl2,glu,glut,i);
 		}
 
-		// draw the shadow
-/*		gl2.glDisable(GL2.GL_LIGHTING);
-		gl2.glEnable(GL2.GL_POLYGON_OFFSET_FILL);
-		gl2.glPolygonOffset(-1.0f, -1.0f);
-
-		float ground[] = { 0.0f, -3.0f, 0.0f };
-		float groundnormal[] = { 0.0f, -1.0f, 0.0f };*/
-
-		//gl2.glPushMatrix();
-		//gl2.glColor3d(0.0, 0.0, 0.0);
-		//projectShadow(gl2, ground, groundnormal, lightpos);
-		//drawcup(gl2, glu, glut, angle);
-
-
-		//gl2.glUseProgram(shaderprogram);
-		/*gl2.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
-		gl2.glEnable(GL2.GL_LIGHTING);
-		float df[] = { 0.3f, 0.3f, 1.0f, 0.0f };*/
-		// set up the matrix transformation - the idea was to create the sign to move up
-		// and rotate to front and center at the end
-		//matrix.glPushMatrix();
-		//System.out.println("in");
-		//transAndRotate(0);
 		if (time < introTime) {
 			//System.out.println(time);
 			time += 1.0f / fps;
@@ -331,6 +344,44 @@ public class CGIntro implements GLEventListener {
 		gl2.glFlush();
 	}
 
+	//	ShadowCup - this is a simple example of using a transformation to create shadows
+	//	Eric McCreath 2009, 2011, 2015
+	// multiply the current matrix with a projection matrix that will do the
+	// shadow
+	static void projectShadow(GL2 gl, float s[], float n[], float l[]) {
+		float w, m;
+		float mat[] = new float[4 * 4];
+
+		w = (s[0] - l[0]) * n[0] + (s[1] - l[1]) * n[1] + (s[2] - l[2]) * n[2];
+		m = l[0] * n[0] + l[1] * n[1] + l[2] * n[2];
+
+		mat[index(0, 0)] = w + n[0] * l[0];
+		mat[index(0, 1)] = n[1] * l[0];
+		mat[index(0, 2)] = n[2] * l[0];
+		mat[index(0, 3)] = -(w + m) * l[0];
+
+		mat[index(1, 0)] = n[0] * l[1];
+		mat[index(1, 1)] = w + n[1] * l[1];
+		mat[index(1, 2)] = n[2] * l[1];
+		mat[index(1, 3)] = -(w + m) * l[1];
+
+		mat[index(2, 0)] = n[0] * l[2];
+		mat[index(2, 1)] = n[1] * l[2];
+		mat[index(2, 2)] = w + n[2] * l[2];
+		mat[index(2, 3)] = -(w + m) * l[2];
+
+		mat[index(3, 0)] = n[0];
+		mat[index(3, 1)] = n[1];
+		mat[index(3, 2)] = n[2];
+		mat[index(3, 3)] = -m;
+
+		gl.glMultMatrixf(mat, 0);
+
+	}
+
+	private static int index(int j, int i) {
+		return j + 4 * i;
+	}
 	public void dispose(GLAutoDrawable glautodrawable) {
 	}
 
